1c33fb168c515c591be5401b1421498deee7b587
rebecoutinho <rebeca.dscoutinho@gmail.com>
2025-12-07 18:33:27 -0300

    Create opcodes_metodos.c

---
+++ src/opcodes_metodos.c
@@ -0,0 +1,17 @@
+#include "opcodes.h"
+#include "engine.h"
+
+void invokestatic_op(JVM* jvm, Frame* frame) {
+    // Lógica do invokestatic copiada do seu código
+}
+
+void ireturn_op(JVM* jvm, Frame* frame) {
+    int32_t ret_val = operand_stack_pop_int(&frame->operand_stack);
+    frame_stack_pop(&jvm->frame_stack);
+    Frame* prev = jvm->frame_stack.top;
+    if (prev) operand_stack_push_int(&prev->operand_stack, ret_val);
+}
+
+void return_op(JVM* jvm, Frame* frame) {
+    frame_stack_pop(&jvm->frame_stack);
+}
